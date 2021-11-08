6f4bbe618792caea44af7637971636e50374f5a6
Swarup Tripathy <64013307+Swarzinium-369@users.noreply.github.com>
2021-11-08 09:02:42 +0530

    Add files via upload

--- 1-Everyday-Java-qa/4-Javatpoint/8-Java Basic Programs/2-Prime number program/first_n_prime.java
+++ 1-Everyday-Java-qa/4-Javatpoint/8-Java Basic Programs/2-Prime number program/first_n_prime.java
@@ -1,21 +1,33 @@
 // Calculation of first n prime numbers 
+import java.util.Scanner;
 
 public class first_n_prime {
     public static void main(String args[]) {
-        int n=5;
-        for(int i=1;i<n;i++){
-            for(int val=2;val<=i;val++){
+        
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = s.nextInt();
+        int num=2;
+        if(n>1){
+            System.out.print("The first "+n+" prime numbers are: ");
+
+            for(int i=1;i<=n;){
                 boolean flag=false;
-                for(int y=2;y<=val/2;y++){
-                    if(val%y==0){
-                        flag=true;
+                for(int j=2;j<=Math.sqrt(num);j++){
+                
+                    if(num%j==0){           // For non prime numbers
+                        flag=true;          // flag changes to true.
                         break;
-                    }
+                    } 
                 }
-                if(!flag)
-                    System.out.print(val+" ");
+                if(!flag){                  // for prime numbers flag remains false
+                    System.out.print(num+" ");
+                    i++;                    // and value of i gets increased by 1
+                }
+                num++;                      // after every iteration the value of num also increases
             }
-            System.out.println();
+        }else{
+            System.out.println("Not valid");
         }
     }
 }
