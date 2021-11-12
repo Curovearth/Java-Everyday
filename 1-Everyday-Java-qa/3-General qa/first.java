8af3a5ba6711b2ad499cfe5d91d07e26561ecf0a
shubhamppl <64500580+shubhamppl@users.noreply.github.com>
2021-11-12 13:59:44 +0530

    added my first program by shubham

---
+++ 1-Everyday-Java-qa/3-General qa/first.java
@@ -0,0 +1,10 @@
+public class Numerics{ 
+public static void main(String[] args) { 
+int x = 10; 
+int y = 12; 
+if(x+y < 10) { 
+System.out.println("x + y is less than 10"); 
+}else { 
+System.out.println("x + y is greater than 20"); } 
+} 
+// less than greater than program
