a00c51f6c251684f186db603545aa041ce136091
Curovearth <mstrider10@gmail.com>
2022-03-29 15:51:32 +0530

    add LinkedList eg

---
+++ 1-Everyday-Java-qa/4-Javatpoint/10-Java Collections/02-linkedlist.java
@@ -0,0 +1,25 @@
+/*
+LinkedList can store the duplicate elements and maintains an insertion order and is not synchronized.
+Here manipulation is fast.
+*/
+
+import java.util.*;
+public class Main{
+  public static void main(String args[]){
+    LinkedList<String> al=new LinkedList<String>();
+    al.add("Ravi");
+    al.add("Vijay");
+    al.add("Ravi");
+    al.add("Ajay");
+    Iterator itr = al.iterator();
+    while(itr.hasNext()){
+      System.out.println(itr.next());
+    }
+  }
+}
+
+// Output is
+// Ravi
+// Vijay
+// Ravi
+// Ajay
